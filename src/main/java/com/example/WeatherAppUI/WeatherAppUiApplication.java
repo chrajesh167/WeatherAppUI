d1188b85f64ceaabcc7e8eb8bfb4e18c24d7101e
chrajesh167 <45836736+chrajesh167@users.noreply.github.com>
2021-08-23 17:11:51 +0530

    Add files via upload

---
+++ src/main/java/com/example/WeatherAppUI/WeatherAppUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeatherAppUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WeatherAppUI/WeatherAppUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WeatherAppUI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
